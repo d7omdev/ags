ac5aff44600ac8253bb24b14c8bd5ed9e3e18818
abdulrahmanDev1 <abdohamdy198461@gmail.com>
2024-09-18 22:09:12 +0300

    .

--- modules/sideleft/toolbox.js
+++ modules/sideleft/toolbox.js
@@ -1,20 +1,20 @@
-import Widget from 'resource:///com/github/Aylur/ags/widget.js';
-const { Box, Label, Scrollable } = Widget;
-import QuickScripts from './tools/quickscripts.js';
-import ColorPicker from './tools/colorpicker.js';
-import Name from './tools/name.js';
+import Widget from 'resource:///com/github/Aylur/ags/widget.js'
+const { Box, Label, Scrollable } = Widget
+import QuickScripts from './tools/quickscripts.js'
+import ColorPicker from './tools/colorpicker.js'
+import Name from './tools/name.js'
 
-export default Scrollable({
+export default Scrollable( {
     hscroll: "never",
     vscroll: "automatic",
-    child: Box({
+    child: Box( {
         vertical: true,
         className: 'spacing-v-10',
         children: [
             QuickScripts(),
             ColorPicker(),
-            Box({ vexpand: true }),
+            Box( { vexpand: true } ),
             Name(),
         ]
-    })
-});
+    } )
+} );
