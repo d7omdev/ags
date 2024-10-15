ba8bffe1fe5cb15b6922a7665e58fa9c814d74ae
d7omdev <abdohamdy198461@gmail.com>
2024-10-15 18:00:33 +0300

    (feat): Added a button to show and copy active windows classes

--- modules/bar/normal/spaceleft.js
+++ modules/bar/normal/spaceleft.js
@@ -1,14 +1,26 @@
 import App from "resource:///com/github/Aylur/ags/app.js";
-import Widget from "resource:///com/github/Aylur/ags/widget.js";
 import Brightness from "../../../services/brightness.js";
 import Indicator from "../../../services/indicator.js";
+import {
+    MenuItem,
+    Label,
+    Scrollable,
+    Box,
+    EventBox,
+    Overlay,
+    Menu,
+    Button
+} from "resource:///com/github/Aylur/ags/widget.js";
+import { MaterialIcon } from '../../.commonwidgets/materialicon.js';
+const { Gdk } = imports.gi
+const { exec, execAsync } = Utils;
 
 const WindowTitle = async () => {
     try {
         const Hyprland = (
             await import("resource:///com/github/Aylur/ags/service/hyprland.js")
         ).default;
-        return Widget.Scrollable({
+        return Scrollable({
             hexpand: true,
             vexpand: true,
             hscroll: "automatic",
@@ -17,7 +29,7 @@ const WindowTitle = async () => {
                 vertical: true,
                 homogeneous: false,
                 children: [
-                    Widget.Label({
+                    Label({
                         xalign: 0,
                         truncate: "end",
                         maxWidthChars: 1, // Doesn't matter, just needs to be non negative
@@ -31,7 +43,7 @@ const WindowTitle = async () => {
                                         : Hyprland.active.client.class;
                             }),
                     }),
-                    Widget.Label({
+                    Label({
                         xalign: 0,
                         truncate: "end",
                         maxWidthChars: 1, // Doesn't matter, just needs to be non negative
@@ -53,9 +65,15 @@ const WindowTitle = async () => {
     }
 };
 
+const Hyprland = (
+    await import("resource:///com/github/Aylur/ags/service/hyprland.js")
+).default;
+const clients = Hyprland.clients;
+
 export default async (monitor = 0, showtitle = false) => {
     const optionalWindowTitleInstance = await WindowTitle();
-    return Widget.EventBox({
+    let menu = null;
+    return EventBox({
         onScrollUp: () => {
             Indicator.popup(1); // Since the brightness and speaker are both on the same window
             Brightness[monitor].screen_value += 0.05;
@@ -67,27 +85,66 @@ export default async (monitor = 0, showtitle = false) => {
         onPrimaryClick: () => {
             App.toggleWindow("sideleft");
         },
-        child: Widget.Box({
-            homogeneous: false,
-            children: [
-                Widget.Box({ className: "bar-corner-spacing" }),
-                Widget.Overlay({
-                    overlays: [
-                        Widget.Box({ hexpand: true }),
-                        Widget.Box({
-                            className: "bar-sidemodule",
-                            hexpand: true,
-                            children: [
-                                Widget.Box({
-                                    vertical: true,
-                                    className: "bar-space-button",
-                                    children: showtitle ? [optionalWindowTitleInstance] : [],
-                                }),
-                            ],
-                        }),
-                    ],
-                }),
-            ],
-        }),
-    });
+
+        child:
+            // Widget.Box({ className: "bar-corner-spacing" }),
+            Overlay({
+                overlays: [
+                    Box({ hexpand: true }),
+                    Box({
+                        className: "bar-sidemodule",
+                        hexpand: false,
+                        children: [
+                            Box({
+                                vertical: true,
+                                className: "bar-space-button",
+                                children: showtitle ? [optionalWindowTitleInstance] : [Button({
+                                    className: 'bar-group bar-group-standalone bar-group-pad-system',
+                                    child: Widget.Box({
+                                        tooltipText: 'Click to copy the class of a window',
+                                        className: "bar-group-margin",
+                                        homogeneous: true,
+                                        child: MaterialIcon('select_window_2', 'norm')
+                                    }),
+                                    onClicked: (button) => {
+                                        if (!menu) {
+                                            menu = Menu({
+                                                className: "menu",
+                                                children: clients.map((client) => {
+                                                    return MenuItem({
+                                                        child: Label({
+                                                            vexpand: true,
+                                                            className: 'text',
+                                                            label: client.class + " • " + (client.title.length > 10 ? client.title.slice(0, 10) + "..." : client.title)
+                                                        }), onActivate: () => {
+                                                            execAsync(`wl-copy ${client.class}`).catch(print);
+                                                            execAsync(`notify-send --icon=info 'Class copied to clipboard' 'Copied class ${client.class}'`).catch(print);
+                                                        },
+
+                                                    });
+                                                }),
+                                            });
+                                        }
+
+                                        try {
+                                            menu.rect_anchor_dy = 4;
+                                            menu.popup_at_widget(
+                                                button,
+                                                Gdk.Gravity.SOUTH,
+                                                Gdk.Gravity.NORTH,
+                                                null
+                                            );
+                                        } catch (error) {
+                                            print(`Error showing menu: ${error}`);
+                                        }
+                                    }
+                                }),],
+                            }),
+
+                        ]
+                    }),
+                ],
+            }),
+    })
 };
+
