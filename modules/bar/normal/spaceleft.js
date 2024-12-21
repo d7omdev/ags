f6d7b3acc130e9dcaa79973f839c5a3e87a32a06
d7omdev <abdohamdy198461@gmail.com>
2024-12-21 14:01:21 +0300

    style: reformat code and update styles
    - Reformatted JavaScript code for better readability and consistency.
    - Updated SCSS styles for `.bar-wintitle-topdesc`, `.bar-space-button > box:first-child`, and `.bar-space-area-rightmost > box`.
    - Fixed color hex codes in `.bar-session-button` and `.bar-session-button:hover`.

--- modules/bar/normal/spaceleft.js
+++ modules/bar/normal/spaceleft.js
@@ -2,92 +2,92 @@ import App from "resource:///com/github/Aylur/ags/app.js";
 import Brightness from "../../../services/brightness.js";
 import Indicator from "../../../services/indicator.js";
 import {
-    Label,
-    Scrollable,
-    Box,
-    EventBox,
-    Overlay,
+  Label,
+  Scrollable,
+  Box,
+  EventBox,
+  Overlay,
 } from "resource:///com/github/Aylur/ags/widget.js";
 
 const WindowTitle = async () => {
-    try {
-        const Hyprland = (
-            await import("resource:///com/github/Aylur/ags/service/hyprland.js")
-        ).default;
-        return Scrollable({
-            hexpand: true,
-            vexpand: true,
-            hscroll: "automatic",
-            vscroll: "never",
-            child: Widget.Box({
-                vertical: true,
-                homogeneous: false,
-                children: [
-                    Label({
-                        xalign: 0,
-                        truncate: "end",
-                        maxWidthChars: 1, // Doesn't matter, just needs to be non negative
-                        className: "txt-smaller bar-wintitle-topdesc txt",
-                        setup: (self) =>
-                            self.hook(Hyprland.active.client, (label) => {
-                                // Hyprland.active.client
-                                label.label =
-                                    Hyprland.active.client.class.length === 0
-                                        ? "Desktop"
-                                        : Hyprland.active.client.class;
-                            }),
-                    }),
-                    Label({
-                        xalign: 0,
-                        truncate: "end",
-                        maxWidthChars: 1, // Doesn't matter, just needs to be non negative
-                        className: "txt-smallie bar-wintitle-txt",
-                        setup: (self) =>
-                            self.hook(Hyprland.active.client, (label) => {
-                                // Hyprland.active.client
-                                label.label =
-                                    Hyprland.active.client.title.length === 0
-                                        ? `Workspace ${Hyprland.active.workspace.id}`
-                                        : Hyprland.active.client.title;
-                            }),
-                    }),
-                ],
-            }),
-        });
-    } catch {
-        return null;
-    }
+  try {
+    const Hyprland = (
+      await import("resource:///com/github/Aylur/ags/service/hyprland.js")
+    ).default;
+    return Scrollable({
+      hexpand: true,
+      vexpand: true,
+      hscroll: "automatic",
+      vscroll: "never",
+      child: Widget.Box({
+        vertical: true,
+        homogeneous: false,
+        children: [
+          Label({
+            xalign: 0,
+            truncate: "end",
+            maxWidthChars: 1, // Doesn't matter, just needs to be non negative
+            className: "txt-smaller bar-wintitle-topdesc txt",
+            setup: (self) =>
+              self.hook(Hyprland.active.client, (label) => {
+                // Hyprland.active.client
+                label.label =
+                  Hyprland.active.client.class.length === 0
+                    ? "Desktop"
+                    : Hyprland.active.client.class;
+              }),
+          }),
+          Label({
+            xalign: 0,
+            truncate: "end",
+            maxWidthChars: 1, // Doesn't matter, just needs to be non negative
+            className: "txt-smallie bar-wintitle-txt",
+            setup: (self) =>
+              self.hook(Hyprland.active.client, (label) => {
+                // Hyprland.active.client
+                label.label =
+                  Hyprland.active.client.title.length === 0
+                    ? `Workspace ${Hyprland.active.workspace.id}`
+                    : Hyprland.active.client.title;
+              }),
+          }),
+        ],
+      }),
+    });
+  } catch {
+    return null;
+  }
 };
 
+export default async (monitor = 0, showtitle = true) => {
+  const optionalWindowTitleInstance = await WindowTitle();
+  return EventBox({
+    onScrollUp: () => {
+      Indicator.popup(1); // Since the brightness and speaker are both on the same window
+      Brightness[monitor].screen_value += 0.05;
+    },
+    onScrollDown: () => {
+      Indicator.popup(1); // Since the brightness and speaker are both on the same window
+      Brightness[monitor].screen_value -= 0.05;
+    },
+    onPrimaryClick: () => {
+      App.toggleWindow("sideleft");
+    },
 
-
-export default async (monitor = 0, showtitle = false) => {
-    const optionalWindowTitleInstance = await WindowTitle();
-    return EventBox({
-        onScrollUp: () => {
-            Indicator.popup(1); // Since the brightness and speaker are both on the same window
-            Brightness[monitor].screen_value += 0.05;
-        },
-        onScrollDown: () => {
-            Indicator.popup(1); // Since the brightness and speaker are both on the same window
-            Brightness[monitor].screen_value -= 0.05;
-        },
-        onPrimaryClick: () => {
-            App.toggleWindow("sideleft");
-        },
-
-        child:
-            // Widget.Box({ className: "bar-corner-spacing" }),
-            Overlay({
-                overlays: [
-                    Box({ hexpand: true }),
-                    Box({
-                        className: "bar-sidemodule",
-                        hexpand: false,
-                        children: showtitle ? [optionalWindowTitleInstance] : [],
-                    }),
-                ],
-            }),
-    })
+    child: Overlay({
+      overlays: [
+        Box({ hexpand: true }),
+        Box({
+          className: "bar-sidemodule",
+          hexpand: false,
+          children: showtitle
+            ? [
+                Widget.Box({ className: "bar-corner-spacing" }),
+                optionalWindowTitleInstance,
+              ]
+            : [],
+        }),
+      ],
+    }),
+  });
 };
-
