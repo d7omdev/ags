cc9cd14c2234ff65aabb65fc6c4777fab9a3d285
d7omdev <abdohamdy198461@gmail.com>
2025-01-27 15:23:09 +0300

    style: update tooltip styles and disable PersonalScripts
    - Updated tooltip styles in `_common.scss`.
    - Commented out `PersonalScripts` in `toolbox.js`.

--- modules/sideleft/toolbox.js
+++ modules/sideleft/toolbox.js
@@ -1,21 +1,21 @@
-import Widget from 'resource:///com/github/Aylur/ags/widget.js'
-const { Box, Label, Scrollable } = Widget
-import QuickScripts, { PersonalScripts } from './tools/quickscripts.js'
-import ColorPicker from './tools/colorpicker.js'
-import Name from './tools/name.js'
+import Widget from "resource:///com/github/Aylur/ags/widget.js";
+const { Box, Label, Scrollable } = Widget;
+import QuickScripts, { PersonalScripts } from "./tools/quickscripts.js";
+import ColorPicker from "./tools/colorpicker.js";
+import Name from "./tools/name.js";
 
 export default Scrollable({
-    hscroll: "never",
-    vscroll: "automatic",
-    child: Box({
-        vertical: true,
-        className: 'spacing-v-10',
-        children: [
-            QuickScripts(),
-            PersonalScripts(),
-            ColorPicker(),
-            Box({ vexpand: true }),
-            Name(),
-        ]
-    })
+  hscroll: "never",
+  vscroll: "automatic",
+  child: Box({
+    vertical: true,
+    className: "spacing-v-10",
+    children: [
+      QuickScripts(),
+      // PersonalScripts(),
+      ColorPicker(),
+      Box({ vexpand: true }),
+      Name(),
+    ],
+  }),
 });
